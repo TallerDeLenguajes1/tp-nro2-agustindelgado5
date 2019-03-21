eb108e7c8df21389c11c9702f3e9d73d4f9cae85
Your Name <you@example.com>
2019-03-21 18:54:58 -0300

    1tp2

---
+++ arreglo1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 4
+#define M 5
+
+
+int void main(){
+	int f,c;
+double  mt[N][M];
+	for(f=0;f<N;f++){
+		for (c= 0; c <M;c++)
+		{
+			cprintf("%lf",mt[f][c]);
+		}
+		cprintf("\n");
+	}	
+};
